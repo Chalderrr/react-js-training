df4be94861c143509815a04c927955277617eb17
Liam Chalder <liam.chalder@laser.red>
2022-08-22 18:13:42 +0100

    :zap: use useState to update title with setTitle

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,26 @@
-import ExpenseDate from "./ExpenseDate";
-import Card from "../UI/Card";
-import "./ExpenseItem.css";
+import React, { useState } from 'react';
 
-function ExpenseItem(props) {
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+import './ExpenseItem.css';
+
+const ExpenseItem = (props) => {
+    const [title, setTitle] = useState(props.title);
+
+    const clickHandler = () => {
+        setTitle('Updated');
+        console.log(title);
+    };
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date} />
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">£{props.amount}</div>
-                <button>Change title</button>
+                <button onClick={clickHandler}>Change title</button>
             </div>
         </Card>
     );
-}
+};
 
 export default ExpenseItem;
