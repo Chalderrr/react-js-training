62fcc13c1d407e267ab000fdcf539dc09bf933c5
Liam Chalder <liam.chalder@laser.red>
2022-08-23 17:06:47 +0100

    :zap: create new object for expense data

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -39,6 +39,12 @@ const ExpenseForm = () => {
     };
     const submitHandler = (event) => {
         event.preventDefault();
+
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate),
+        };
     };
 
     return (
