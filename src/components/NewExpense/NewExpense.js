37c2d263a0f1d199423b7fd0ae89a97e106e42a2
Liam Chalder <liam.chalder@laser.red>
2022-09-27 16:49:31 +0100

    :zap: hide/show form and also cancel out new expense

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,20 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './NewExpense.css';
 import ExpenseForm from './ExpenseForm';
 
 const NewExpense = (props) => {
+    const [hideForm, setHideForm] = useState(false);
+
     const onSaveExpenseDataHandler = (enteredExpenseData) => {
         const expenseData = {
             ...enteredExpenseData,
             id: Math.random().toString(),
         };
         props.onAddExpense(expenseData);
+        setHideForm(false);
+    };
+
+    const handleFormVisibility = () => {
+        setHideForm(true);
+    };
+
+    const stopEditingHandler = () => {
+        setHideForm(false);
     };
 
     return (
         <div className="new-expense">
-            <ExpenseForm onSaveExpenseData={onSaveExpenseDataHandler} />
+            {!hideForm && (
+                <button onClick={handleFormVisibility}>Add New Expense</button>
+            )}
+            {hideForm && (
+                <ExpenseForm
+                    onSaveExpenseData={onSaveExpenseDataHandler}
+                    onCancel={stopEditingHandler}
+                />
+            )}
         </div>
     );
 };
