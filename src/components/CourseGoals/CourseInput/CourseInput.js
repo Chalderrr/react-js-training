58bdaefbc6f0dcd7555fc2c3f8bf7074ae04ad07
Liam Chalder <liam.chalder@laser.red>
2022-09-28 11:14:39 +0100

    :lipstick: update input styles if empty

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -28,7 +28,14 @@ const CourseInput = (props) => {
                 <label style={{ color: !isValid ? 'red' : 'black' }}>
                     Course Goal
                 </label>
-                <input type="text" onChange={goalInputChangeHandler} />
+                <input
+                    style={{
+                        borderColor: !isValid ? 'red' : 'grey',
+                        backgroundColor: !isValid ? 'salmon' : 'transparent',
+                    }}
+                    type="text"
+                    onChange={goalInputChangeHandler}
+                />
             </div>
             <Button type="submit">Add Goal</Button>
         </form>
