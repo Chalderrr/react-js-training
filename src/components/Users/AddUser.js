7cfb783a43088336ee0df5ba3be5aee7299310b4
Liam Chalder <liam.chalder@laser.red>
2022-10-18 14:10:24 +0100

    :zap: take state from usrname and age and handle validation on it

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from '../UI/Card';
 import Button from '../UI/Button';
 
 const AddUser = (props) => {
+    const [enteredUsername, setEnteredUsername] = useState('');
+    const [enteredAge, setEnteredAge] = useState('');
+
     const addUserHandler = (event) => {
         event.preventDefault();
+
+        if (
+            enteredUsername.trim().length === 0 ||
+            enteredAge.trim().length === 0
+        ) {
+            return;
+        }
+        if (+enteredAge < 1) {
+            return;
+        }
+
+        console.log(enteredUsername, enteredAge);
+
+        setEnteredUsername('');
+        setEnteredAge('');
+    };
+
+    const usernameChangeHandler = (event) => {
+        if (event.target.value.length > 0) {
+            setEnteredUsername(event.target.value);
+        }
+    };
+
+    const ageChangeHandler = (event) => {
+        if (event.target.value.length > 0) {
+            setEnteredAge(event.target.value);
+        }
     };
 
     return (
@@ -17,7 +47,9 @@ const AddUser = (props) => {
                     type="text"
                     id="username"
                     name="name"
+                    value={enteredUsername}
                     className="block w-full border border-[#cccccc] focus:border-[#4f005f] mb-3"
+                    onChange={usernameChangeHandler}
                 />
                 <label htmlFor="age" className="block mb-2 font-bold">
                     Age (Years)
@@ -26,7 +58,9 @@ const AddUser = (props) => {
                     type="number"
                     id="age"
                     name="age"
+                    value={enteredAge}
                     className="block w-full border border-[#cccccc] focus:border-[#4f005f] mb-3"
+                    onChange={ageChangeHandler}
                 />
                 <Button
                     type="submit"
