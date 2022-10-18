4f95cfc595927083626af8beb6546d31c8902afb
Liam Chalder <liam.chalder@laser.red>
2022-10-18 12:20:13 +0100

    :zap: add tailwind and start applying tailwind classes

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -7,12 +7,26 @@ const AddUser = (props) => {
     };
 
     return (
-        <Card className="bg-white">
+        <Card className="bg-white shadow-md rounded-lg p-4 my-8 mx-auto max-w-2xl w-[90%]">
             <form onSubmit={addUserHandler}>
-                <label htmlFor="username">Username</label>
-                <input type="text" id="username" name="name" />
-                <label htmlFor="age">Age (Years)</label>
-                <input type="number" id="age" name="age" />
+                <label htmlFor="username" className="block mb-3 font-bold">
+                    Username
+                </label>
+                <input
+                    type="text"
+                    id="username"
+                    name="name"
+                    className="block w-full border border-[#cccccc] focus:border-[#4f005f] mb-3"
+                />
+                <label htmlFor="age" className="block mb-3 font-bold">
+                    Age (Years)
+                </label>
+                <input
+                    type="number"
+                    id="age"
+                    name="age"
+                    className="block w-full border border-[#cccccc] focus:border-[#4f005f] mb-3"
+                />
                 <button type="submit">Add User</button>
             </form>
         </Card>
