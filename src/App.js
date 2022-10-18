dc1d2ee8f7ea31d725d02829f0ca196cd28c6bc5
Liam Chalder <liam.chalder@laser.red>
2022-10-18 15:10:46 +0100

    :zap: add a setter that gets previous userlist and adds new data to it

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import AddUser from './components/Users/AddUser';
 import UserList from './components/Users/UserList';
 
 function App() {
     const [usersList, setUsersList] = useState([]);
 
+    const addUserHandler = (userName, userAge) => {
+        setUsersList((prevUsersList) => {
+            return [...prevUsersList, { name: userName, age: userAge }];
+        });
+    };
+
     return (
         <div>
-            <AddUser onAddUser={}/>
-            <UserList users={usersList}/>
+            <AddUser onAddUser={addUserHandler} />
+            <UserList users={usersList} />
         </div>
     );
 }
