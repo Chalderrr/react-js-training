9a4711d991c06dd832ca8bdf919fec017232672f
Liam Chalder <liam.chalder@laser.red>
2022-10-18 12:30:40 +0100

    :zap: update gitignore and move button to UI component

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Card from '../UI/Card';
+import Button from '../UI/Button';
 
 const AddUser = (props) => {
     const addUserHandler = (event) => {
@@ -9,7 +10,7 @@ const AddUser = (props) => {
     return (
         <Card className="bg-white shadow-md rounded-lg p-4 my-8 mx-auto max-w-2xl w-[90%]">
             <form onSubmit={addUserHandler}>
-                <label htmlFor="username" className="block mb-3 font-bold">
+                <label htmlFor="username" className="block mb-2 font-bold">
                     Username
                 </label>
                 <input
@@ -18,7 +19,7 @@ const AddUser = (props) => {
                     name="name"
                     className="block w-full border border-[#cccccc] focus:border-[#4f005f] mb-3"
                 />
-                <label htmlFor="age" className="block mb-3 font-bold">
+                <label htmlFor="age" className="block mb-2 font-bold">
                     Age (Years)
                 </label>
                 <input
@@ -27,7 +28,13 @@ const AddUser = (props) => {
                     name="age"
                     className="block w-full border border-[#cccccc] focus:border-[#4f005f] mb-3"
                 />
-                <button type="submit">Add User</button>
+                <Button
+                    type="submit"
+                    className="border border-[#4f005f] text-white bg-[#4f005f] py-1 px-4 hover:bg-[#741188] hover:border-[#741188]"
+                    onClick={props.onClick}
+                >
+                    Add User
+                </Button>
             </form>
         </Card>
     );
