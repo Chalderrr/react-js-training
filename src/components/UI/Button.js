379a991666b27d2aad6efe9227bb1706e9968428
Liam Chalder <liam.chalder@laser.red>
2022-10-28 16:42:43 +0100

    :zap: make sure button component has a click event

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 const Button = (props) => {
     return (
-        <button type={props.type || 'button'} className={`${props.className}`}>
+        <button
+            type={props.type || 'button'}
+            className={`${props.className}`}
+            onClick={props.onClick}
+        >
             {props.children}
         </button>
     );
