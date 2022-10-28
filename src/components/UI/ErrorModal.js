cb49a1356cbd37cb885504e2ab5407da73b4b52c
Liam Chalder <liam.chalder@laser.red>
2022-10-28 16:44:30 +0100

    :zap: add onReset prop to click events on the ErrorModal

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -5,7 +5,10 @@ import Button from './Button';
 const ErrorModal = (props) => {
     return (
         <div>
-            <div className="fixed top-0 left-0 w-full h-screen z-10 bg-[rgba(0,0,0,0.75)]"></div>
+            <div
+                className="fixed top-0 left-0 w-full h-screen z-10 bg-[rgba(0,0,0,0.75)]"
+                onClick={props.onReset}
+            />
             <div className="bg-white shadow-md rounded-lg mx-auto max-w-2xl w-[90%] fixed top-[30vh] left-1/2 -translate-x-1/2 w-[80%] z-[100] overflow-hidden">
                 <header className="bg-[#4f005f] p-8">
                     <h2 className="text-white m-0 text-xl font-bold uppercase">
@@ -16,7 +19,11 @@ const ErrorModal = (props) => {
                     <p>{props.message}</p>
                 </div>
                 <footer className="flex justify-end p-8">
-                    <Button className="border border-[#4f005f] text-white bg-[#4f005f] py-1 px-4 hover:bg-[#741188] hover:border-[#741188]">
+                    <Button
+                        type="button"
+                        className="border border-[#4f005f] text-white bg-[#4f005f] py-1 px-4 hover:bg-[#741188] hover:border-[#741188]"
+                        onClick={props.onReset}
+                    >
                         Okay
                     </Button>
                 </footer>
