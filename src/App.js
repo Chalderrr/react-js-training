7a195dc1b535a8482d0ae214dc791c5e90cae660
Liam Chalder <liam.chalder@laser.red>
2022-10-28 16:44:42 +0100

    :fire: remove ErrorModal from App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import AddUser from './components/Users/AddUser';
 import UserList from './components/Users/UserList';
-import ErrorModal from './components/UI/ErrorModal';
 
 function App() {
     const [usersList, setUsersList] = useState([]);
