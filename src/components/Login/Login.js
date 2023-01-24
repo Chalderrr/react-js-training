6aa2c74ad042a28c2180f963d4d516f000f7ac5f
Liam Chalder <liam.chalder@laser.red>
2023-01-24 18:21:12 +0000

    :lipstick: reintroduce useContext

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -47,33 +47,31 @@ const Login = (props) => {
         isValid: false,
     });
 
-    // useEffect(() => {
-    //     // Assign setTimeout to a function that can be returned with a clearTimeout();
-    //     // This prevents the useEffect from endlessly running the setFormIsValid handler when the state is updated
-    //     // Which in turn reduces a large amount of HTTP requests
-    //     const identifier = setTimeout(() => {
-    //         console.log('Checking for validity!');
-    //     }, 500);
-    //     setFormIsValid(
-    //         enteredEmail.includes('@') && enteredPassword.trim().length > 6
-    //     );
-    //
-    //     return () => {
-    //         console.log('clearing...');
-    //         clearTimeout(identifier);
-    //     };
-    // }, [enteredEmail, enteredPassword]); // Set dependency functions that need re-evaluating, if they change, then the useEffect will re-run
+    useEffect(() => {
+        // Assign setTimeout to a function that can be returned with a clearTimeout();
+        // This prevents the useEffect from endlessly running the setFormIsValid handler when the state is updated
+        // Which in turn reduces a large amount of HTTP requests
+        const identifier = setTimeout(() => {
+            console.log('Checking for validity!');
+        }, 500);
+        setFormIsValid(emailState.isValid && passwordState.isValid);
+
+        return () => {
+            console.log('clearing...');
+            clearTimeout(identifier);
+        };
+    }, [emailState, passwordState]); // Set dependency functions that need re-evaluating, if they change, then the useEffect will re-run
 
     const emailChangeHandler = (event) => {
         // setEnteredEmail(event.target.value);
         // Run reducer dispatch to return an object with a type and a value in this case 'USER_INPUT' and event.target.value
         dispatchEmail({ type: 'USER_INPUT', val: event.target.value });
 
-        setFormIsValid(
-            event.target.value.includes('@') &&
-                // enteredPassword.trim().length > 6
-                passwordState.isValid
-        );
+        // setFormIsValid(
+        //     event.target.value.includes('@') &&
+        //         // enteredPassword.trim().length > 6
+        //         passwordState.isValid
+        // );
     };
 
     const passwordChangeHandler = (event) => {
@@ -81,9 +79,9 @@ const Login = (props) => {
         // Run reducer dispatch to return an object with a type and a value in this case 'USER_INPUT' and event.target.value
         dispatchPassword({ type: 'USER_INPUT', val: event.target.value });
 
-        setFormIsValid(
-            emailState.isValid && event.target.value.trim().length > 6
-        );
+        // setFormIsValid(
+        //     emailState.isValid && event.target.value.trim().length > 6
+        // );
     };
 
     const validateEmailHandler = () => {
