582b5b9a3fcb3a7cb2dc977d861ae9d630aab8bd
Liam Chalder <liam.chalder@laser.red>
2023-01-24 15:20:32 +0000

    :lipstick: adding a useEffect as a side effect to run only relevant execution

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -8,10 +8,18 @@ function App() {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
 
     // This will create an infinite loop if the state is updated in the function
-    const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
-    if (storedUserLoggedInInformation === '1') {
-        setIsLoggedIn(true);
-    }
+    // const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
+    // if (storedUserLoggedInInformation === '1') {
+    //     setIsLoggedIn(true);
+    // }
+
+    useEffect(() => {
+        const storedUserLoggedInInformation =
+            localStorage.getItem('isLoggedIn');
+        if (storedUserLoggedInInformation === '1') {
+            setIsLoggedIn(true);
+        }
+    }, []);
 
     const loginHandler = (email, password) => {
         // We should of course check email and password
