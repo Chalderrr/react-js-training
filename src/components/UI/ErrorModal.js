f4ea2dd58fd08b42bac6b297eeebea58d9232dda
Liam Chalder <liam.chalder@laser.red>
2023-01-24 11:34:04 +0000

    :zap: add some root elements to attach overlays to

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -9,7 +9,7 @@ const ErrorModal = (props) => {
                 className="fixed top-0 left-0 w-full h-screen z-10 bg-[rgba(0,0,0,0.75)]"
                 onClick={props.onReset}
             />
-            <div className="bg-white shadow-md rounded-lg mx-auto max-w-2xl w-[90%] fixed top-[30vh] left-1/2 -translate-x-1/2 w-[80%] z-[100] overflow-hidden">
+            <div className="bg-white shadow-md rounded-lg mx-auto max-w-xl w-[90%] fixed top-[30vh] left-1/2 -translate-x-1/2 w-[80%] z-[100] overflow-hidden">
                 <header className="bg-[#4f005f] p-8">
                     <h2 className="text-white m-0 text-xl font-bold uppercase">
                         {props.title}
