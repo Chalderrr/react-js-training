85bee2099d951f3c2d234b3596cf101091385d33
Liam Chalder <liam.chalder@laser.red>
2023-01-25 17:48:37 +0000

    :lipstick: add useState to handle showing/closing cart

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
 
 function App() {
+    const [cartIsShown, setCardIsShown] = useState(false);
+
+    const showCartHandler = () => {
+        setCardIsShown(true);
+    };
+
+    const hideCartHandler = () => {
+        setCardIsShown(false);
+    };
+
     return (
         <Fragment>
-            <Cart />
-            <Header />
+            {cartIsShown && <Cart onClose={hideCartHandler} />}
+            <Header onShowCart={showCartHandler} />
             <main>
                 <Meals />
             </main>
